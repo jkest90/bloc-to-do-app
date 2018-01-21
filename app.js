f1429b8ab3992f31d4b5f864f57c53f20a6fd372
jkest90 <jkest618@gmail.com>
2018-01-20 19:35:00 -0700

    finish checkpoint

---
+++ app.js
@@ -0,0 +1,45 @@
+// 1.
+
+function onReady() {
+    const addToDoForm = document.getElementById('addToDoForm');
+    const newToDoText = document.getElementById('newToDoText');
+    const toDoList = document.getElementById('toDoList');
+
+    addToDoForm.addEventListener('submit', event => {
+        console.log('list item submitted');
+        // prevent default submit event of page reload/redirect
+        event.preventDefault();
+
+        // get input text
+        let title = newToDoText.value;
+
+        // create a new li
+        let newLi = document.createElement('li');
+
+        // create a new input
+        let checkbox = document.createElement('input');
+
+        // set the input's type to checkbox
+        checkbox.type = 'checkbox';
+
+        // set the content of newLi to input value
+        newLi.textContent = title;
+
+        // attach the checkbox to li
+        newLi.appendChild(checkbox);
+
+        // attach the li to the ul
+        toDoList.appendChild(newLi);
+
+        // empty the input
+        newToDoText.value = '';
+
+    });
+}
+
+window.onLoad = function() {
+    alert('The window has loaded!');
+    onReady();
+};
+
+window.onLoad();
